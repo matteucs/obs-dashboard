00c204fbcf5972ff8c148971d8f41c857f094946
matteucs <matthew.eucalitto@gmail.com>
2026-06-25 07:19:36 -0400

    Update generate-brief.js

--- api/generate-brief.js
+++ api/generate-brief.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   const authHeader = req.headers.authorization;
   if (req.method !== 'GET' && req.method !== 'POST') return res.status(405).end();
 
